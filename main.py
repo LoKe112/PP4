72dcffa8621606ab1e79b2309b26e4084d47fe15
LoKe112 <ivan1200100@mail.ru>
2023-12-18 16:21:33 +0400

    pep8 + fix graphs

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ def get_processed_df(path: str) -> pd.DataFrame:
     df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
     if not ((df.isnull().sum()).eq(0).all()):
         df.dropna(inplace=True, ignore_index=True)
-    curestd,curemedian = df['Value'].mean(),df['Value'].median()
+    curestd, curemedian = df['Value'].mean(), df['Value'].median()
     df['MedianDeviation'] = abs(curemedian-df['Value'])
     df['StdDeviation'] = abs(curestd-df['Value'])
     return df
+
 
 def get_statistical_info(df: pd.DataFrame, parametr: str) -> pd.Series:
     """Getting statistical information
@@ -31,7 +32,8 @@ def get_statistical_info(df: pd.DataFrame, parametr: str) -> pd.Series:
     """
     if parametr in df.columns:
         return df[parametr].describe()
-      
+
+
 def std_deviation_filtration(df: pd.DataFrame, std_deviation: float) -> pd.DataFrame:
     """Filtering std deviation
     Args:
@@ -41,6 +43,7 @@ def std_deviation_filtration(df: pd.DataFrame, std_deviation: float) -> pd.DataF
       Dataframe with deviation
     """
     return df[df["StdDeviation"] >= std_deviation]
+
 
 def date_filtration(df: pd.DataFrame, start_date: str, end_date: str) -> pd.DataFrame:
     """Filtering by date
@@ -54,39 +57,42 @@ def date_filtration(df: pd.DataFrame, start_date: str, end_date: str) -> pd.Data
     start_date = pd.to_datetime(start_date, format='%Y-%m-%d')
     end_date = pd.to_datetime(end_date, format='%Y-%m-%d')
     return df[(start_date <= df["Date"]) & (df["Date"] <= end_date)]
-  
+
+
 def group_by_month_with_average_value(df: pd.DataFrame) -> pd.Series:
     """Grouping by month with calculation of the average value
     Args:
       df: Dataframe with original values
-      
+
     Returns:
       A series indicating the average value for all months
     """
-    
+
     return df.groupby(df.Date.dt.month)["Value"].mean()
-  
+
+
 def show_value_graph(df: pd.DataFrame) -> None:
     """Show of the value for the entire period
     Args:
       df: Dataframe with original values
-      
+
     """
-   
+
     fig = plt.figure(figsize=(19, 5))
     plt.ylabel("Value")
     plt.xlabel("date")
     plt.title('Курс долара(американские горки в конце)')
     plt.plot(df["Date"], df["Value"], color='blue',
-                linestyle='-', linewidth=1)
+             linestyle='-', linewidth=1)
     plt.show()
-    
+
+
 def show_value_graph_median_average(df: pd.DataFrame, month: int) -> None:
     """Showing graph for the specified month
     Args:
       df: Dataframe with original values
       month: The month for which the temperature graph is drawn
-      
+
     """
     month_df = df[(df.Date.dt.month == month)]
     fig = plt.figure(figsize=(18, 8))
@@ -108,7 +114,7 @@ def show_value_graph_median_average_year(df: pd.DataFrame, month: int, year: int
     Args:
       df: Dataframe with original values
       month: The month for which the temperature graph is drawn
-      
+
     """
     month_df = df[(df.Date.dt.month == month) & (df.Date.dt.year == year)]
     fig = plt.figure(figsize=(18, 8))
@@ -123,12 +129,12 @@ def show_value_graph_median_average_year(df: pd.DataFrame, month: int, year: int
     plt.axhline(y=month_df["Value"].median(
     ), color='black', label="Median")
     plt.legend(loc=2, prop={'size': 8})
-    
 
     plt.show()
 
+
 df = get_processed_df("dataset.csv")
-      
+
 
 print(get_processed_df("dataset.csv"))
 
